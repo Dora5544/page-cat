e6c2309ada8b8a293017922a79c5dad0484619e2
BetterMeDing <9376523+better-me-ding@user.noreply.gitee.com>
2023-12-02 21:56:40 +0800

    add the function to let sidepanel sends message to conten.js

--- src/pages/Panel/Panel.tsx
+++ src/pages/Panel/Panel.tsx
@@ -4,7 +4,10 @@ import './Panel.css';
 const Panel: React.FC = () => {
   const [pageTitle, setPageTitle] = useState('...');
 
+  console.log("before useEffect")
+
   useEffect(() => {
+    console.log("in useEffect")
     // 定义消息监听器
     const messageListener = (message: any) => {
       if (message.type === 'page-information') {
@@ -17,6 +20,16 @@ const Panel: React.FC = () => {
     // 添加消息监听器
     chrome.runtime.onMessage.addListener(messageListener);
 
+    // sidepanel.js
+
+    // 发送消息到用户标签页
+    chrome.tabs.query({ active:true, currentWindow: true }, function (tabs) {
+      const activeTab = tabs[0];
+      chrome.tabs.sendMessage(Number(activeTab.id), { type: 'messageFromSidePanel', data: 'Hello from Side Panel!' });
+    });
+
+
+    console.log("panel send")
     // 在组件卸载时移除消息监听器
     return () => {
       chrome.runtime.onMessage.removeListener(messageListener);
